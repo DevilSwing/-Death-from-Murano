1d25798f11b2ed94f0b5ef55a32910c1d4f082e3
DevilSwing <melky025@mail.ru>
2018-04-26 17:15:36 +0300

    gg

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     plus.click(function () {
         var $input = $(this).parent().find('input');
         $input.val(parseInt($input.val()) + 1);
-        if ($input.val() == 100) {
+        if ($input.val() == 10) {
             $input.val(1);
         }else {
             $input.change();
@@ -22,9 +22,56 @@ $(document).ready(function () {
     });
 
 
+var price = 0;
+
+let	check1 = $('#10'),
+	check2 = $('#11'),
+	check3 = $('#12'),
+	check4 = $('#13'),
+	check5 = $('#14'),
+	check6 = $('#15'),
+	check7 = $('#16');
+
+$('.choise').on('change', function () {
+if (check1.checked) {
+price = price + 100;
+} else {
+price = price - 100;
+}
+if (check2.checked) {
+price = price + 100;
+} else {
+price = price - 100;
+}
+if (check3.checked) {
+price = price + 100;
+} else {
+price = price - 100;
+}
+if (check4.checked) {
+price = price + 100;
+} else {
+price = price - 100;
+}
+if (check5.checked) {
+price = price + 100;
+} else {
+price = price - 100;
+}
+if (check6.checked) {
+price = price + 100;
+} else {
+price = price - 100;
+}
+if (check7.checked) {
+price = price + 100;
+} else {
+price = price - 100;
+}
 
 
 
+});
 
 
 
@@ -53,35 +100,3 @@ $(document).ready(function () {
 
 
 
-
-
-
-// 	  $(function(){
-// 		$('.choice c1').click(function(){
-// 			  	$('.cost-span').text('1000')
-// 			  });
-// 	  });
-	  
-
-// 	  $('#1000').on('change', function () {
-//     if ($(this).attr('checked')) {
-//         $('#text').text('0 тнг.');
-//     } else {
-//         $('#text').text('1000 тнг.');
-//     }
-
-// $('.minus').click(function () {
-//         var $input = $(this).parent().find('input');
-//         var count = parseInt($input.val()) - 1;
-//         count = count < 1 ? 1 : count;
-//         $input.val(count);
-//         $input.change();
-//         return false;
-//     });
-//     $('.plus').click(function () {
-//         var $input = $(this).parent().find('input');
-//         $input.val(parseInt($input.val()) + 1);
-//         $input.change();
-//         return false;
-//     });
- 
